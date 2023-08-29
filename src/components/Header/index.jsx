a7d5c21f289ef33ab5e27bf6cd3c4b9fd2a99c9b
EduardoFagotti <fagotti.edu567@outlook.com>
2023-08-29 17:57:56 -0300

    header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import { Container } from "./styles";
+
+export default function Header() {
+  return (
+    <Container>
+      <div className="cont">
+        <Image with="" heigth="" alt="" />
+        <div>
+          <button>TESTE</button>
+          <button>TESTE</button>
+          <button>TESTE</button>
+          <button>TESTE</button>
+        </div>
+      </div>
+    </Container>
+  );
+}
---
+++ src/components/Header/styles.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 80px;
+  background-color: #131313;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .cont {
+    width: 80%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border-bottom: 2px solid #7f7f7f;
+  }
+`;
