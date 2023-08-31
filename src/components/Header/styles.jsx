fffa9a9ae06507cc5d2128643ba8dc38cc7dbd21
EduardoFagotti <fagotti.edu567@outlook.com>
2023-08-31 20:56:45 -0300

    create components

--- src/components/Header/styles.jsx
+++ src/components/Header/styles.jsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 100%;
-  height: 80px;
+  height: auto;
   background-color: #131313;
   display: flex;
   justify-content: center;
@@ -10,11 +10,37 @@ export const Container = styled.div`
 
   .cont {
     width: 80%;
-    height: 100%;
     display: flex;
-    flex-direction: column;
-    justify-content: center;
     align-items: center;
+    justify-content: space-between;
+    padding-bottom: 10px;
+    padding-top: 10px;
     border-bottom: 2px solid #7f7f7f;
+
+    img {
+      width: 60px;
+      height: 60px;
+    }
+
+    .buttons {
+      width: 50%;
+      display: flex;
+      justify-content: space-around;
+    }
+
+    button {
+      padding: 10px;
+      color: #7f7f7f;
+      background-color: transparent;
+      border: none;
+      font-size: 18px;
+      font-family: "Roboto", sans-serif;
+      font-weight: bold;
+    }
+
+    button:hover {
+      color: #9041c5;
+      cursor: pointer;
+    }
   }
 `;
