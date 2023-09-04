29b5e29bc11b6306fdfe9aabe89b9b8dac1d02ae
EduardoFagotti <fagotti.edu567@outlook.com>
2023-09-03 21:25:07 -0300

    update: footer

--- src/components/Footer/styles.jsx
+++ src/components/Footer/styles.jsx
@@ -1,14 +1,40 @@
 import { styled } from "styled-components";
 
 export const Container = styled.div`
+  font-family: "Roboto", sans-serif;
   width: 100%;
   height: 300px;
   background-color: #080808;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
   .cont {
     display: flex;
+    justify-content: space-between;
     width: 80%;
     padding-top: 20px;
     padding-bottom: 20px;
+    color: #fff;
+
+    @media (min-width: 1200px) {
+      width: 60%;
+    }
+
+    div {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+    }
+
+    .contacts {
+      align-items: start;
+    }
+  }
+
+  img {
+    width: 150px;
+    height: 150px;
   }
 `;
