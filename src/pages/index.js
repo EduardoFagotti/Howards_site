0ceb887fc70a9a5528c42dd5ac25fdacce51aee8
Eduardo Fagotti <fagotti.edu567@outlook.com>
2023-10-09 16:01:02 -0300

    update meta description

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
     <>
       <Head>
         <title>Howards</title>
-        <meta name="description" content="teste" />
+        <meta name="description" content="creating responsive websites" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/icon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
