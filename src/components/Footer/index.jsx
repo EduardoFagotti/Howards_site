02370632a976a06012864bb82a4baecd7359db17
Eduardo Fagotti <fagotti.edu567@outlook.com>
2023-10-09 16:30:30 -0300

    update SMS link

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -17,9 +17,15 @@ export default function Footer() {
           <p>Howards</p>
         </div>
         <div className="contacts">
-          <p>
-            <MdSms className="icon" /> +1 561-334-7604
-          </p>
+          <Link
+            className="icon"
+            href="https://criarmeulink.com.br/u/1696879756"
+            target="_blank"
+          >
+            <p>
+              <MdSms className="icon" /> +1 561-334-7604
+            </p>
+          </Link>
           <Link className="link" href="https://wa.link/kh6g4v" target="_blank">
             <p>
               <IoLogoWhatsapp className="icon" /> +1 561-334-7604
